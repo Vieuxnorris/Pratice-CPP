33fa2da2eb2de46b0b817d6b4a2cd4ebabdf3155
Vieuxnorris <vieuxnorris@gmail.com>
2022-06-21 15:00:53 +0200

    update header

--- CPP_Data_Algo/Cpp_Prime.h
+++ CPP_Data_Algo/Cpp_Prime.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-
+// Basique Base Cpp Prime
 void A_Simple_Cpp_Program(void);
 void Fundamental_types(void);
 void Pointers(void);
@@ -16,3 +16,6 @@ void Local_and_Global_Scopes(void);
 void Namespace(void);
 void Traditional_c_style_casting(void);
 void Static_Casting(void);
+void Implicit_Casting(void);
+
+// Basique Controle Flow Cpp Prime
