e9086643a805e6d860afa64556c544a0b4cb270a
Vieuxnorris <vieuxnorris@gmail.com>
2022-06-21 14:49:44 +0200

    update

--- CPP_Data_Algo/Traditional C_Style Casting.cpp
+++ CPP_Data_Algo/Traditional C_Style Casting.cpp
@@ -5,4 +5,10 @@ void Traditional_c_style_casting()
 	int cat = 14;
 	double dog = (double)cat; // traditional C_style cast
 	double pig = double(cat); // C++ functional cast
+
+	int i1 = 18;
+	int i2 = 16;
+	double dv1 = i1 / i2; // dv1 has value 1.0
+	double dv2 = double(i1) / double(i2);
+	double dv3 = double(i1 / i2);
 }
