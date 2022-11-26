3670ca17d483ea229a807b30b0c45a349685e9e2
Vieuxnorris <vieuxnorris@gmail.com>
2022-11-26 15:19:15 +0100

    ajout de function recursion.cpp

--- CPP_Data_Algo/Recursion.cpp
+++ CPP_Data_Algo/Recursion.cpp
@@ -1,4 +1,5 @@
 #include "Array_Linked_List_Recursion.h"
+#include <vector>
 
 int recursiveFatorial(int n)						// recursive factorial function
 {
@@ -6,6 +7,14 @@ int recursiveFatorial(int n)						// recursive factorial function
 		return 1;
 	else
 		return n * recursiveFatorial(n - 1);		// recursive case
+}
+
+int BinaryFib(int k)
+{
+	if (k <= 1)
+		return k;
+	else
+		return BinaryFib(k - 1) + BinaryFib(k - 2);
 }
 
 void drawOneTick(int tickLength, int ticklabel = -1)
@@ -35,6 +44,38 @@ int LinearSum(int A[], int n)
 		return LinearSum(A, n - 1) + A[n - 1];
 }
 
+int BinarySum(int A[], int i, int n)
+{
+	if (n == 1)
+		return A[i];
+	return BinarySum(A, i, (n / 2)) + BinarySum(A, i + (n / 2), (n / 2));
+}
+
+int ReverseArray(int A[], int i, int j)
+{
+	if (i < j)
+	{
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+		ReverseArray(A, i + 1, j - 1);
+	}
+	return 0;
+}
+
+int IterativeReverseArray(int A[], int i, int j)
+{
+	while (i < j)
+	{
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+		i += 1;
+		j -= 1;
+	}
+	return 0;
+}
+
 void drawRuler(int nInches, int majorLength)
 {
 	drawOneTick(majorLength, 0);
@@ -50,6 +91,8 @@ int Recursion()
 	int A[4] = { 1,2,3,4};
 	//std::cout << recursiveFatorial(4) << std::endl;
 	drawTicks(3);
-	std::cout << LinearSum(A, 4);
+	std::cout << LinearSum(A, 4) << std::endl;
+	std::cout << ReverseArray(A, 0, 3) << std::endl;
+	std::cout << BinarySum(A, 0, 4) << std::endl;
 	return EXIT_SUCCESS;
 }
